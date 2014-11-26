f538f6240c1b4a6546dc7e369323df3737bf77b8
iheartkode <y2kmark73@gmail.com>
2014-11-26 08:34:39 -0800

    Added learning jQuery and random quotes updates

---
+++ Learning jQuery/js/main.js
@@ -0,0 +1,15 @@
+(function() {
+  $('button').click(function() {
+    $('#h').fadeIn( 500 );
+    $('#e').delay( 500 ).fadeIn( 500 );
+    $('#l').delay( 1000 ).fadeIn( 500 );
+    $('#l2').delay( 1500 ).fadeIn( 500 );
+    $('#o').delay( 2000 ).fadeIn( 500 );
+    $('#w').delay( 2500 ).fadeIn( 500 );
+    $('#o2').delay( 3000 ).fadeIn( 500 );
+    $('#r').delay( 3500 ).fadeIn( 500 );
+    $('#l3').delay( 4000 ).fadeIn( 500 );
+    $('#d').delay( 4500 ).fadeIn( 500 );
+  });
+})();
+    
