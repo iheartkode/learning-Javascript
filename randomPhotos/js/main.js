f58717f3a71235800b8a3c03af504d4315b20cb1
iheartkode <y2kmark73@gmail.com>
2014-12-27 12:25:18 -0800

    Added project randomPhotos

---
+++ randomPhotos/js/main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Mark Howard on 12/26/2014.
+ */
+$(document).ready(function() {
+    var images = _.sample([
+        '<img src="img/snowymtn.jpg" alt="Old Farm House" height="500" class="farm"/>',
+        '<img src="img/farm.jpg" alt="Old Farm House" height="500" class="farm"/>',
+        '  <img src="img/river.jpg" alt="River" height="500" class="river" />',
+        ' <img src="img/road.jpg" alt="A Road" height="500" class="road"/>',
+        ' <img src="img/desert.jpg" alt="A desert" height="500" class="desert"/>',
+        '<img src="img/woods.jpg" alt="Wood Landscape" height="500" class="woods"/>',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/f/fitwalk/01/l/1391008345vuabp.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/n/Natureworks/03/l/139404735343e9t.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/a/Archbob/11/l/1383461209nfki7.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/d/DMedina/06/l/14041601315pps2.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/a/AcrylicArtist/10/l/1414002734e614m.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/k/KittyBitty/04/l/1365144183vs7g2.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/j/JohnEdwards/03/l/13645850300hw7n.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/t/thesuccess/04/l/1397908115obnpd.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/k/kconnors/09/l/1378587258f9ofy.jpg" height="500" />',
+        '<img src="http://cdn.morguefile.com/imageData/public/files/p/Prawny/05/l/14007127543n7kz.jpg" height="500" />',
+
+    ]);
+    $('#image').append(images);
+});
+
+
+
+
+
+
+
+
+
